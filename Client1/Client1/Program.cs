0dd8469f46e4b29692bac6a81fb4fc99f12ef96d
Soep <kgh010529@naver.com>
2020-11-27 12:16:43 +0900

    first commit

---
+++ Client1/Client1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContinuousClient cc = new MyContinuousClient();
+            cc.Run();
+        }
+    }
+
+    class MyContinuousClient
+    {
+        TcpClient client = null;
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("========== CLIENT1 ==========");
+                Console.WriteLine("1. 서버 연결하기");
+                Console.WriteLine("2. 메세지 전송하기");
+                Console.WriteLine("3. 서버 구동 여부 확인");
+                Console.WriteLine("============================");
+
+                string key = Console.ReadLine();
+                int order = 0;
+
+                // 입력받은 key의 값을 int.TryParse를 사용해 int형으로 형변환해줍니다.
+                // 형변환에 성공하면 order에 변화된 값이 저장됩니다.
+                // 형변환에 실패하면 false를 리턴합니다.
+                if (int.TryParse(key, out order))
+                {
+                    switch (order)
+                    {
+                        case 1:
+                            {
+                                if (client != null)
+                                {
+                                    Console.WriteLine("이미 서버연결 했거등");
+                                    Console.ReadKey();
+                                }
+                                else
+                                {
+                                    Connect();
+                                }
+                                break;
+                            }
+
+                        case 2:
+                            {
+                                if (client == null)
+                                {
+                                    Console.WriteLine("서버부터 연결하셈");
+                                    Console.ReadKey();
+                                }
+
+                                else
+                                {
+                                    SendMessage();
+                                }
+
+                                break;
+                            }
+
+                        case 3:
+                            {
+                                if (client == null)
+                                {
+                                    Console.WriteLine("서버부터 연결하셈");
+                                    Console.ReadKey();
+                                }
+
+                                else
+                                {
+                                    SendFlag();
+                                }
+
+                                break;
+                            }
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Wrong Input");
+                    Console.ReadKey();
+                }
+
+                Console.Clear();
+            }
+        }
+
+        private void SendMessage()
+        {
+            Console.WriteLine("Input the message to send");
+            string message = Console.ReadLine();
+            byte[] byteData = new byte[message.Length];
+            byteData = Encoding.Default.GetBytes(message);
+
+            client.GetStream().Write(byteData, 0, byteData.Length);
+            Console.WriteLine("Success to send!");
+            Console.ReadKey();
+        }
+
+        private void SendFlag()
+        {
+            string massage = "!";
+            byte[] byteData = new byte[massage.Length];
+            byteData = Encoding.Default.GetBytes(massage);
+
+            client.GetStream().Write(byteData, 0, byteData.Length);
+            Console.ReadKey();
+        }
+
+        private void Connect()
+        {
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 9999);
+            Console.WriteLine("Success to connect to server and input your message");
+            Console.ReadKey();
+        }
+    }
+
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AsynchronousServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyServer server = new MyServer();
+        }
+    }
+
+    class MyServer
+    {
+        public MyServer()
+        {
+            AsyncServerStart();
+        }
+
+        private void AsyncServerStart()
+        {
+            TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Any, 9999));
+            listener.Start();
+            Console.WriteLine("서버를 시작합니다.");
+
+            while (true)
+            {
+                TcpClient acceptClient = listener.AcceptTcpClient();
+                ClientData clientData = new ClientData(acceptClient);
+                clientData.client.GetStream().BeginRead(clientData.readByteData, 0, clientData.readByteData.Length, new AsyncCallback(DataReceived), clientData);
+
+
+
+            }
+        }
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            ClientData callbackClient = ar.AsyncState as ClientData;
+            int byteRead = callbackClient.client.GetStream().EndRead(ar);
+            string readString = Encoding.Default.GetString(callbackClient.readByteData, 0, byteRead);
+            Console.WriteLine("{0}번 사용자 : {1}", callbackClient.clientNumber, readString);
+            callbackClient.client.GetStream().BeginRead(callbackClient.readByteData, 0, callbackClient.readByteData.Length, new AsyncCallback(DataReceived), callbackClient);
+
+        }
+    }
+
+    class ClientData
+    {
+        public TcpClient client { get; set; }
+        public byte[] readByteData { get; set; }
+        public int clientNumber;
+
+        public ClientData(TcpClient client)
+        {
+            this.client = client;
+            this.readByteData = new byte[1024];
+
+            string clientEndPoint = client.Client.LocalEndPoint.ToString();
+            char[] point = { '.', ':' };
+            string[] splitedData = clientEndPoint.Split(point);
+            this.clientNumber = int.Parse(splitedData[3]);
+            Console.WriteLine("{0}번 사용자 접속성공", clientNumber);
+
+        }
+    }
+}
